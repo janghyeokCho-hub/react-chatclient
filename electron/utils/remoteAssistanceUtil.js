e5b448260a8c95b12ecd09d0789e6c3c35e27f62
kjmin <kjmin@covision.co.kr>
2021-08-17 11:17:29 +0900

    경동 원격실행 패치누락 핫픽스

--- electron/utils/remoteAssistanceUtil.js
+++ electron/utils/remoteAssistanceUtil.js
@@ -1,59 +1,59 @@
+import { app } from 'electron';
+import { spawn } from 'child_process';
+import { resolve, dirname } from 'path';
 import logger from './logger';
 
 export const connectRemoteViewer = (sessionKey) => {
-    logger.info(`./remote/KoinoViewer.exe -un ${sessionKey} -i 128.1.1.36 -p 7002 -p2 7003 -fr 1572863 -t 30`);
-    var spawn = require('child_process').spawn, ls = spawn(`cd remote && KoinoViewer.exe -un ${sessionKey} -i 128.1.1.36 -p 7002 –p2 7003 -fr 1572863 -t 3000`, [], { shell: true })
-    ls.stdout.once('data', function (data) {
-        logger.info('KoinoViewer.exe returned data: ' + data);
-    });
+  const appPath = dirname(app.getAppPath());
+  const remotePath = resolve(appPath, 'remote', 'KoinoViewer.exe');
 
-    ls.stderr.once('data', function (data) {
-        logger.info('KoinoViewer.exe occured error: ' + data);
-    });
+  logger.info(`Spawn ${remotePath} -un ${sessionKey} -i 128.1.1.36 -p 7002 –p2 7003 -fr 1572863 -t 3000`);
+  const remoteProcess = spawn(`${remotePath} -un ${sessionKey} -i 128.1.1.36 -p 7002 –p2 7003 -fr 1572863 -t 3000`, [], { shell: true });
 
-    ls.once('exit', function (code) {
-        logger.info('KoinoViewer.exe has terminated with code: ' + code);
-    });
+  remoteProcess.once('error', (err) => {
+    logger.info('Failed to spawn KoinoViewer : ', err);
+  })
+
+  remoteProcess.once('exit', function (code) {
+    logger.info('KoinoViewer.exe has terminated with code: ' + code);
+  });
+
+  remoteProcess.stdout.once('data', function (data) {
+    logger.info('KoinoViewer.exe returned data: ' + data);
+  });
+
+  remoteProcess.stderr.once('data', function (data) {
+    logger.info('KoinoViewer.exe occured error: ' + data);
+  });
 }
 
 export const connectRemoteHost = (sessionKey) => {
-    logger.info(`./remote/KoinoHostLauncher.exe -un ${sessionKey} -i 128.1.1.36 -p 7002 -p2 7003 -fr 1572863 -t 30`);
-    var path = require('path');
-    var p = path.join('./remote','KoinoHostLauncher.exe');
-    var spawn = require('child_process').spawn, ls = spawn(p,[
-        '-un', sessionKey,
-        '-i', '128.1.1.36',
-        '-p', '7002',
-        '-p2', '7003',
-        '-fr', '1572863',
-        '-t', '30'
-    ])
-    // var spawn = require('child_process').spawn, ls = spawn(`cd remote && KoinoHostLauncher.exe -un ${sessionKey} -i 128.1.1.36 -p 7002 –p2 7003 -fr 1572863 -t 3000`, [], { shell: true })
-    ls.stdout.once('data', function (data) {
-        logger.info('KoinoHostLaunch.exe returned data: ' + data);
-    });
+  const appPath = dirname(app.getAppPath());
+  const remotePath = resolve(appPath, 'remote', 'KoinoHostLauncher.exe');
 
-    ls.stderr.once('data', function (data) {
-        logger.info('KoinoHostLaunch.exe occured error: ' + data);
-    });
+  logger.info(`Spawn ${remotePath} -un ${sessionKey} -i 128.1.1.36 -p 7002 –p2 7003 -fr 1572863 -t 3000`);
+  const remoteProcess = spawn(remotePath, [
+    '-un', sessionKey,
+    '-i', '128.1.1.36',
+    '-p', '7002',
+    '-p2', '7003',
+    '-fr', '1572863',
+    '-t', '30'
+  ]);
 
-    ls.once('exit', function (code) {
-        logger.info('KoinoHostLaunch.exe has terminated with code: ' + code);
-    });
+  remoteProcess.once('error', (err) => {
+    logger.info('Failed to spawn KoinoHostLauncher : ', err);
+  });
+
+  remoteProcess.once('exit', function (code) {
+    logger.info('KoinoHostLaunch.exe has terminated with code: ' + code);
+  });
+
+  remoteProcess.stdout.once('data', function (data) {
+    logger.info('KoinoHostLaunch.exe returned data: ' + data);
+  });
+
+  remoteProcess.stderr.once('data', function (data) {
+    logger.info('KoinoHostLaunch.exe occured error: ' + data);
+  });
 }
-
-export const getPath = () => {
-    var spawn = require('child_process').spawn, ls = spawn(`cd`, [], { shell: true })
-    ls.stdout.once('data', function (data) {
-        logger.info('Get remote path: ', data);
-    });
-
-    ls.stderr.once('data', function (data) {
-        console.log('Get remote path stderr: ' + data);
-    });
-
-    ls.once('exit', function (code) {
-        console.log('Get remote path exit: ' + code);
-    });
-}
-
