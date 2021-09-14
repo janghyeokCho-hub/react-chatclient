4e93ff60adde281fa939bb74d65c0293d4790e3b
Kyoungjun Min <kjmin@covision.co.kr>
2021-09-14 01:30:34 +0000

    Resolve "멘션 표기오류 및 검색오류"

--- src/lib/profile.js
+++ src/lib/profile.js
@@ -1,5 +1,19 @@
 import { chatsvr, managesvr } from '@/lib/api';
+import LRU from 'lru-cache';
 
-export const getProfileInfo = targetId => {
-  return managesvr('get', `/profile/${targetId}`);
+const cache = new LRU({
+  max: 20,
+  maxAge: 1000 * 60 * 60 // 60m (1h)
+});
+
+export const getProfileInfo = async targetId => {
+  const cachedData = cache.get(targetId);
+  if (cachedData) {
+    // Cache hit
+    return cachedData;
+  }
+  // Fetch data
+  const profileData = await managesvr('get', `/profile/${targetId}`);
+  cache.set(targetId, profileData);
+  return profileData;
 };
