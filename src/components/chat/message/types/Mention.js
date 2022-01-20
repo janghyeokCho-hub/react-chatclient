9ecf28c458165099c2dfdeba0109d44c5b3a68ec
Cho Jang-Hyeok <janghyeok9257@gmail.com>
2022-01-20 17:19:26 +0900

    자기 자신 맨션 클릭시 검색시 URL -> @이름 으로 변경

--- src/components/chat/message/types/Mention.js
+++ src/components/chat/message/types/Mention.js
@@ -5,13 +5,13 @@ import Plain from '@C/chat/message/types/Plain';
 import { getJobInfo } from '@/lib/common';
 import useMemberInfo from '@/hooks/useMemberInfo';
 
-const Mention = ({ marking, mentionInfo, type, targetId}) => {
+const Mention = ({ marking, mentionInfo, type, targetId }) => {
   const dispatch = useDispatch();
   const [value, setValue] = useState('');
   const [memberInfo, setMemberInfo] = useState(null);
-  
+
   const _isMounted = useRef(true);
-  
+
   const { findMemberInfo } = useMemberInfo();
 
   const setMention = async () => {
@@ -19,22 +19,21 @@ const Mention = ({ marking, mentionInfo, type, targetId}) => {
       if (mentionInfo && Array.isArray(mentionInfo)) {
         const memberInfo = await findMemberInfo(mentionInfo, targetId);
         let txt = '@Unknown';
-        if(!memberInfo) {
+        if (!memberInfo) {
           //
-        }
-        else if (memberInfo.name) {
+        } else if (memberInfo.name) {
           const jobInfo = getJobInfo(memberInfo);
           txt = `@${jobInfo}`;
         } else if (memberInfo.id) {
           txt = `@${memberInfo.id}`;
         }
-        if(_isMounted.current === true) {
+        if (_isMounted.current === true) {
           setValue(txt);
           setMemberInfo(memberInfo);
         }
       }
     }
-  }
+  };
 
   useEffect(() => {
     setMention();
@@ -47,17 +46,18 @@ const Mention = ({ marking, mentionInfo, type, targetId}) => {
       setValue(null);
       setMemberInfo(null);
       _isMounted.current = false;
-    }
+    };
   }, []);
 
   const handleClick = () => {
     if (memberInfo.isMine) {
       if (typeof covi.changeSearchView == 'function') {
         // eumtalk://mention. ~ 전체검색하는 경우에만 검색결과가 나오지 않음
-        // covi.changeSearchView(`eumtalk://mention.user.${memberInfo.id}`);
-
         // 멘션 임시패치
-        covi.changeSearchView(`://mention.user.${memberInfo.id}`);
+        // covi.changeSearchView(`://mention.user.${memberInfo.id}`);
+
+        // 멘션 자기 자신 검색시 url 대신 이름으로 변경
+        covi.changeSearchView(`@${getJobInfo(memberInfo)}`);
       }
     } else {
       openProfilePopup(dispatch, memberInfo.id);
