fb44fe654ac034d0e3c7bf4a15e9db47966bde1e
Kyoungjun Min <kjmin@covision.co.kr>
2022-04-21 06:32:07 +0000

    Resolve "채널 tempMessage / 자동로그인 하위호환 대응"

--- src/lib/login.js
+++ src/lib/login.js
@@ -1,4 +1,6 @@
 import { chatsvr, managesvr } from '@/lib/api';
+import { getConfig } from './util/configUtil';
+
 const { _loginRequest } = require(`@/lib/${DEVICE_TYPE}/login`);
 
 /**
@@ -10,10 +12,17 @@ const { _loginRequest } = require(`@/lib/${DEVICE_TYPE}/login`);
  * Request Header 추가시 MAC Address 값 암호화
  */
 
-export const loginRequest = params => {
-  const url = params?.isAuto ? '/na/r/m/login' : '/na/m/login';
-  delete params.isAuto;
-  return _loginRequest('post', url, params, {
+export const loginRequest = async params => {
+  const isLegacyAutoLoginAPI =
+    (getConfig('Legacy_AutoLogin') || false) === true;
+  const url =
+    !isLegacyAutoLoginAPI && params?.isAuto ? '/na/r/m/login' : '/na/m/login';
+  if (isLegacyAutoLoginAPI) {
+    isLegacyAutoLoginAPI && delete params.al;
+    isLegacyAutoLoginAPI && delete params.nip_slevel;
+    params?.isAuto && delete params.isAuto;
+  }
+  const res = await _loginRequest('post', url, params, {
     /**
      * 2021.01.21
      * MAC Address 전송 활성화시 true로 변경하기
@@ -21,11 +30,21 @@ export const loginRequest = params => {
     useMACAddress: true,
     useMACEncryption: true,
   });
+  return res;
 };
 
 export const extLoginRequest = params => {
-  const url = params?.isAuto ? '/na/r/m/extlogin' : '/na/m/extlogin';
-  delete params.isAuto;
+  const isLegacyAutoLoginAPI =
+    (getConfig('Legacy_AutoLogin') || false) === true;
+  const url =
+    !isLegacyAutoLoginAPI && params?.isAuto
+      ? '/na/r/m/extlogin'
+      : '/na/m/extlogin';
+  if (isLegacyAutoLoginAPI) {
+    isLegacyAutoLoginAPI && delete params.al;
+    isLegacyAutoLoginAPI && delete params.nip_slevel;
+    params?.isAuto && delete params.isAuto;
+  }
   return _loginRequest('post', url, params, {
     // useMACAddress 비활성화시 useMACEncryption 사용 안함
     useMACAddress: true,
