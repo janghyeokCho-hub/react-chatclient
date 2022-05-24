e5b657513addc334d1662f8ffcff389aaf7eae3a
Kyoungjun Min <kjmin@covision.co.kr>
2022-05-24 02:35:40 +0000

    Resolve "[SaaS] 회사별 sys_config 정책분리 대응"

--- src/lib/b/login.js
+++
@@ -1,9 +0,0 @@
-import { managesvr } from '@/lib/api';
-
-function _loginRequest_browser(method, path, params) {
-    return managesvr(method, path, params);
-}
-
-export {
-    _loginRequest_browser as _loginRequest
-}
