8678518113c0510c9028c954243983e886e28c84
Kyoungjun Min <kjmin@covision.co.kr>
2022-09-29 04:26:17 +0000

    Resolve "링크 썸네일 사이드이펙트 오류보완"

--- electron/event/common.js
+++ electron/event/common.js
@@ -1,12 +1,19 @@
 import { BrowserWindow, screen } from 'electron';
 import url from 'url';
 import path from 'path';
-import { getSubPopupBound } from '../utils/commonUtils';
+import LRU from 'lru-cache';
 import axios from 'axios';
 import cheerio from 'cheerio';
-import { updateLinkInfo } from './appData';
 import { detect as detectCharset } from 'jschardet';
 import iconv from 'iconv-lite';
+import { updateLinkInfo } from './appData';
+import { getSubPopupBound } from '../utils/commonUtils';
+import logger from '../utils/logger';
+
+const cache = new LRU({
+  max: 15,
+  ttl: 1000 * 60 * 60, // 1 hour
+});
 
 export const setRoomWinMap = (event, args) => {
   ROOM_WIN_MAP[args.roomID] = args.winID;
@@ -272,130 +279,105 @@ export const setBeforePresence = (event, args) => {
   BEFORE_PRESENCE = args;
 };
 
-export const getUrlGraphData = (event, args) => {
+function requestHead(url) {
+  if (cache.get(`Head_${url}`)) {
+    return cache.get(`Head_${url}`);
+  }
+  return axios.head(url);
+}
+
+async function requestPage(url) {
+  if (cache.get(`Page_${url}`)) {
+    return cache.get(`Page_${url}`);
+  }
+  const { headers } = await requestHead(url);
+  if (headers['content-type'].indexOf('text/html') > -1) {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    // 원본 데이터의 charset 확인
+    const responseCharset = detectCharset(response.data);
+
+    /**
+     * charset이 없거나 / utf-8인 경우에 Decoding 생략
+     * charset이 확인 가능한 경우 decoding 수행
+     */
+    const needToDecode =
+      responseCharset &&
+      responseCharset.encoding &&
+      responseCharset.encoding.toLowerCase() !== 'utf-8';
+    const decoded = needToDecode
+      ? iconv.decode(response.data, responseCharset.encoding)
+      : response.data.toString();
+    const data = iconv.encode(decoded, 'utf-8');
+
+    // 파싱(크롤링) 시작
+    const $ = cheerio.load(data);
+    const hostPattern =
+      /^((http[s]?):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/i;
+    const graphData = $('head>meta[property^="og:"]');
+    let graphObj = {};
+
+    if (graphData != null && graphData.length > 0) {
+      graphData.each((i, elm) => {
+        let key = $(elm).attr('property').replace('og:', '');
+        let content = $(elm).attr('content');
+        graphObj[key] = content;
+      });
+    }
+    graphObj.domain = url.replace(hostPattern, '$2://$3');
+
+    if (!Object.prototype.hasOwnProperty.call(graphObj, 'title')) {
+      // title이 없을 시 페이지의 title 입력
+      graphObj.title = $('title').text();
+    }
+
+    if (!Object.prototype.hasOwnProperty.call(graphObj, 'description')) {
+      // description 없을 시 페이지의 description 입력
+      const desc = $('[name="description"]');
+      if (desc != null && desc.length > 0) {
+        graphObj.description = desc.first().attr('content');
+      } else {
+        graphObj.description = '여기를 눌러 링크를 확인하세요';
+      }
+    }
+
+    cache.set(`Page_${url}`, graphObj);
+    return graphObj;
+  }
+}
+
+export const getUrlGraphData = async (event, args) => {
   //args : messageId, url
   const saveLinkInfo = async (messageId, linkInfo) => {
     await updateLinkInfo(messageId, linkInfo);
   };
 
-  axios
-    .head(args.url)
-    .then(({ headers }) => {
-      // response type 이 html 일 경우에만 link 해석
-      if (headers['content-type'].indexOf('text/html') > -1) {
-        axios
-          .get(args.url, { responseType: 'arraybuffer' })
-          .then(response => {
-            // 원본 데이터의 charset 확인
-            const responseCharset = detectCharset(response.data);
+  try {
+    const graphObj = await requestPage(args.url);
+    console.log('graphObj', args, graphObj);
+    if (Object.prototype.hasOwnProperty.call(graphObj, 'title')) {
+      const returnObj = {
+        link: args.url,
+        thumbNailInfo: graphObj,
+      };
 
-            /**
-             * charset이 없거나 / utf-8인 경우에 Decoding 생략
-             * charset이 확인 가능한 경우 decoding 수행
-             */
-            const needToDecode =
-              responseCharset &&
-              responseCharset.encoding &&
-              responseCharset.encoding.toLowerCase() !== 'utf-8';
-            const decoded = needToDecode
-              ? iconv.decode(response.data, responseCharset.encoding)
-              : response.data.toString();
-            const data = iconv.encode(decoded, 'utf-8');
+      // link info
 
-            // 파싱(크롤링) 시작
-            const $ = cheerio.load(data);
-            const hostPattern =
-              /^((http[s]?):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/i;
-            const graphData = $('head>meta[property^="og:"]');
-            let returnObj = null;
-            let graphObj = {};
+      // link data send renderer
+      event.sender.send(args.returnChannel, {
+        messageId: args.messageId,
+        roomId: args.roomId,
+        linkInfo: returnObj,
+      });
 
-            if (graphData != null && graphData.length > 0) {
-              graphData.each((i, elm) => {
-                let key = $(elm).attr('property').replace('og:', '');
-                let content = $(elm).attr('content');
-                graphObj[key] = content;
-              });
-            }
+      // link data local db 저장
 
-            graphObj.domain = args.url.replace(hostPattern, '$2://$3');
-
-            if (!Object.prototype.hasOwnProperty.call(graphObj, 'title')) {
-              // title이 없을 시 페이지의 title 입력
-              graphObj.title = $('title').text();
-            }
-
-            if (
-              !Object.prototype.hasOwnProperty.call(graphObj, 'description')
-            ) {
-              // description 없을 시 페이지의 description 입력
-              const desc = $('[name="description"]');
-              if (desc != null && desc.length > 0) {
-                graphObj.description = desc.first().attr('content');
-              } else {
-                graphObj.description = '여기를 눌러 링크를 확인하세요';
-              }
-            }
-
-            if (Object.prototype.hasOwnProperty.call(graphObj, 'title')) {
-              returnObj = {
-                link: args.url,
-                thumbNailInfo: graphObj,
-              };
-
-              // link info
-
-              // link data send renderer
-              event.sender.send(args.returnChannel, {
-                messageId: args.messageId,
-                roomId: args.roomId,
-                linkInfo: returnObj,
-              });
-
-              // link data local db 저장
-
-              saveLinkInfo(args.messageId, returnObj);
-              event.sender.send(args.returnChannel, {
-                messageId: args.messageId,
-                roomId: args.roomId,
-                linkInfo: returnObj,
-              });
-            } else {
-              // 아무 처리 없음
-              event.sender.send(args.returnChannel, {
-                messageId: args.messageId,
-                roomId: args.roomId,
-                linkInfo: null,
-              });
-
-              // link data local db 저장 ( 링크 데이터 없음 )
-              saveLinkInfo(args.messageId, {});
-            }
-          })
-          .catch(e => {
-            // 아무 처리 없음
-            event.sender.send(args.returnChannel, {
-              messageId: args.messageId,
-              roomId: args.roomId,
-              linkInfo: null,
-            });
-
-            // link data local db 저장 ( 링크 데이터 없음 )
-            saveLinkInfo(args.messageId, {});
-          });
-      } else {
-        // 아무 처리 없음
-        event.sender.send(args.returnChannel, {
-          messageId: args.messageId,
-          roomId: args.roomId,
-          linkInfo: null,
-        });
-
-        // link data local db 저장 ( 링크 데이터 없음 )
-        saveLinkInfo(args.messageId, {});
-      }
-    })
-    .catch(e => {
+      saveLinkInfo(args.messageId, returnObj);
+      event.sender.send(args.returnChannel, {
+        messageId: args.messageId,
+        roomId: args.roomId,
+        linkInfo: returnObj,
+      });
+    } else {
       // 아무 처리 없음
       event.sender.send(args.returnChannel, {
         messageId: args.messageId,
@@ -405,7 +387,20 @@ export const getUrlGraphData = (event, args) => {
 
       // link data local db 저장 ( 링크 데이터 없음 )
       saveLinkInfo(args.messageId, {});
+    }
+  } catch (err) {
+    // 아무 처리 없음
+    event.sender.send(args.returnChannel, {
+      messageId: args.messageId,
+      roomId: args.roomId,
+      linkInfo: null,
     });
+
+    // link data local db 저장 ( 링크 데이터 없음 )
+    saveLinkInfo(args.messageId, {});
+
+    logger.info('[GetUrlGraphData] Error: ' + JSON.stringify(err));
+  }
 };
 
 export const reqSaveError = (event, args, logger) => {
